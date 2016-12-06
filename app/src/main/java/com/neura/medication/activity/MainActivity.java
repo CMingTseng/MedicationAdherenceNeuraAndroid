13490f5f899ded295a887bbcdf565c7c89a5ac3f
hadas <hadasmeitav@gmail.com>
2016-12-06 19:17:50 +0200

    Initial commit

---
+++ app/src/main/java/com/neura/medication/activity/MainActivity.java
@@ -0,0 +1,50 @@
+package com.neura.medication.activity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.neura.medication.R;
+import com.neura.medicationaddon.NeuraManager;
+import com.neura.standalonesdk.util.SDKUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final View loginButton = findViewById(R.id.login_with_neura);
+
+        /**
+         * Init Neura instance. If you have an {@link android.app.Application} class, better call
+         * {@link NeuraManager#initNeuraConnection(Context, String, String)} from the Application's onCreate.
+         */
+        NeuraManager.getInstance().initNeuraConnection(getApplicationContext(),
+                getString(R.string.app_uid), getString(R.string.app_secret));
+
+        if (SDKUtils.isConnected(this, NeuraManager.getInstance().getNeuraClient())) {
+            loginButton.setVisibility(View.GONE);
+        } else {
+            loginButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    NeuraManager.getInstance().authenticateWithNeura(MainActivity.this, new Handler.Callback() {
+                        @Override
+                        public boolean handleMessage(Message message) {
+                            if (message.arg1 == 1) {
+                                loginButton.setVisibility(View.GONE);
+                            }
+                            return false;
+                        }
+                    });
+                }
+            });
+        }
+    }
+
+}
---
+++ app/src/main/java/com/neura/medication/activity/SamplePillsReceiver.java
@@ -0,0 +1,72 @@
+package com.neura.medication.activity;
+
+import android.content.Context;
+
+import com.neura.medication.R;
+import com.neura.medicationaddon.ActionPillsReceiver;
+
+/**
+ * When an event is received from neura(userWokeUp, userGotUp, userLeftHome) there's a default behaviour
+ * of displaying a notification with a default text, for example : when neura detects 'userWokeUp',
+ * we'll be displaying a notification for taking your morning pill,
+ * it looks like this : https://s17.postimg.org/69i9g6ky7/Good_Morning_Pill.png
+ * The notification is done from {@link ActionPillsReceiver}, on
+ * {@link ActionPillsReceiver#handlePillActionReceived(Context, String)}.
+ * You have 2 options :
+ * 1. If you want {@link ActionPillsReceiver} to show the notifications for you :
+ * a. Define SamplePillsReceiver which extends {@link ActionPillsReceiver}, and define this receiver
+ * in your AndroidManifest file like so :
+ * <receiver android:name=".activity.SamplePillsReceiver">
+ * <intent-filter>
+ * <action android:name="com.neura.medicationaddon.MorningPill" />
+ * <action android:name="com.neura.medicationaddon.EveningPill" />
+ * <action android:name="com.neura.medicationaddon.PillBoxReminder" />
+ * <p>
+ * <action android:name="com.neura.medication.ACTION_NOTIFICATION_TOOK_PILLS" />
+ * <action android:name="com.neura.medication.ACTION_NOTIFICATION_REMIND_ME_LATER" />
+ * <p>
+ * </intent-filter>
+ * </receiver>
+ * b. override the methods : {@link ActionPillsReceiver#getNotificationSmallIcon(Context)},
+ * {@link ActionPillsReceiver#getMorningPillIcon(Context)}, {@link ActionPillsReceiver#getEveningPillIcon(Context)},
+ * {@link ActionPillsReceiver#getPillBoxReminderIcon(Context)}, and give the icons you wish to set
+ * in the notification.
+ * 2. If you don't want to use the default notification display, override the method
+ * {@link ActionPillsReceiver#handlePillActionReceived(Context, String)}
+ * and handle the events yourself. In this case, you don't need to override the methods from section 1b.
+ */
+public class SamplePillsReceiver extends ActionPillsReceiver {
+
+    /**
+     * Option 1 - let Neura handle the events, and set your own icons to be displayed in the notification.
+     */
+    @Override
+    protected int getNotificationSmallIcon(Context context) {
+        return R.mipmap.ic_launcher;
+    }
+
+    @Override
+    protected int getMorningPillIcon(Context context) {
+        return R.mipmap.morningicon;
+    }
+
+    @Override
+    protected int getEveningPillIcon(Context context) {
+        return R.mipmap.evening_icon;
+    }
+
+    @Override
+    protected int getPillBoxReminderIcon(Context context) {
+        return R.mipmap.pillboxicon;
+    }
+
+    /**
+     * Option 2 - implement your own mechanism for handling events received from Neura.
+     * This is just a sample, so it's not doing anything, and calling the base receiver.
+     */
+    @Override
+    protected void handlePillActionReceived(Context context, String actionPill) {
+        super.handlePillActionReceived(context, actionPill);
+    }
+
+}
